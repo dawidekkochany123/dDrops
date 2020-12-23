db1551e73145d7367059a49d3ce0e989d44fd66b
dawidekkochany123 <dawidekkochany123@gmail.com>
2020-12-23 13:58:42 +0100

    First Drop class

---
+++ src/main/java/dawidos506/ddrops/objects/Drop.java
@@ -0,0 +1,17 @@
+package dawidos506.ddrops.objects;
+
+import org.bukkit.inventory.ItemStack;
+
+public class Drop {
+
+    public ItemStack drop;
+    public int amount;
+    public double chance;
+
+    public Drop(ItemStack drop, int amount, double chance) {
+        this.drop = drop;
+        this.amount = amount;
+        this.chance = chance;
+    }
+
+}
